835af158c675c72d34d201a2560ccf76ad205c8b
aryans <locaalprince22@gmail.com>
2020-08-11 11:14:48 +0545

    Node-server-localhost

---
+++ index.js
@@ -0,0 +1,61 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+
+const hostname = "localhost";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    // console.log(req.headers);
+
+    console.log('request for' + req.url + 'by method' + req.method);
+
+    if (req.method == 'GET') {
+        var fileURL;
+        if (req.url == '/') {
+            fileURL = "/index.html"
+        }
+        else {
+            fileURL = req.url
+        }
+        var filePath = path.resolve('./public' + fileURL);
+
+        const fileExt = path.extname(filePath);
+
+        if (fileExt == '.html') {
+            fs.exists(filePath, (exists) => {
+                if (!exists) {
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end('<html> <body> <h1> error 404 : ' + fileURL + ' does not exists.</h1></body> </html>');
+                }
+                res.statusCode = 404;
+                res.setHeader('Content-Tyoe', 'text/html');
+                fs.createReadStream(filePath).pipe(res);
+
+            })
+        }
+        else {
+            res.statusCode = 404;
+            res.setHeader('Content-Tyoe', 'text/html');
+            res.end('<html> <body> <h1>error 404: ' + fileURL + ' not a HTML file.</h1></body> </html>');
+        }
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Tyoe', 'text/html');
+        res.end('<html> <body> <h1>error 404: ' + fileURL + ' Not Supported.</h1></body> </html>');
+
+    }
+
+
+    // res.statusCode = 200;
+    // res.setHeader('Content-Tyoe', 'text/html');
+    // res.end('<html> <body> <h1>Server Connection Successful.</h1></body> </html>')
+
+
+});
+
+server.listen(port, hostname, () => {
+    console.log(`srever running at http://${hostname}:${port}`);
+});
